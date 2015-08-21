26a6cade1eef8348faded805922ba555c5ce47fb
Fleen Mobile <FleenMobile@gmail.com>
2015-08-21 15:07:43 +0200

    Add splash screen and fonts
    I added splash screen with the photo of Lanckorona and all RobotoFonts available throughout the entire app

--- Application/SpacerPoLanckoronie/app/src/main/java/com/fleenmobile/spacerpolanckoronie/activities/HistoryActivity.java
+++ Application/SpacerPoLanckoronie/app/src/main/java/com/fleenmobile/spacerpolanckoronie/activities/HistoryActivity.java
@@ -1,23 +1,24 @@
-package com.fleenmobile.spacerpolanckoronie;
+package com.fleenmobile.spacerpolanckoronie.activities;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.fleenmobile.spacerpolanckoronie.R;
 
-public class SplashScreen extends ActionBarActivity {
+public class HistoryActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash_screen);
+        setContentView(R.layout.activity_history);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_splash_screen, menu);
+        getMenuInflater().inflate(R.menu.menu_history, menu);
         return true;
     }
 
